fcd392fb5515f71341195ba0fb2b3588fcbe7a4f
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2024-09-24 14:03:41 -0500

    Added semi colon

--- New_Folder/Hello_World.js
+++ New_Folder/Hello_World.js
@@ -1,2 +1,8 @@
 var greeting = 'Hello, World!';
-    console.log(greeting)
+console.log(greeting);
+
+var greeting ='Welcome to Node.js';
+console.log(greeting2);
+
+var sentence1 ='Learning to debug code with the debugger';
+console.log(sentenceOne);
